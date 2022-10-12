cbe615f599e6532671fced5341bac112dffd8373
Venkateshprasadh <93995646+Venkateshprasadh@users.noreply.github.com>
2022-10-12 15:43:14 +0530

    Deploy Application on Cloud.

---
+++ Phase -5 CloudDeploy/src/main/java/com/example/demo/DemoController.java
@@ -0,0 +1,15 @@
+package com.example.demo;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.ResponseBody;
+
+@Controller
+public class DemoController {
+@ResponseBody
+@RequestMapping("/display")
+	public String display() {
+		return "<h1>Application deployed on cloud <h1>";
+	}
+	
+}
